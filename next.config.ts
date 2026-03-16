ffa6c487927044bad841486912ab1a69f8e10cbc
marioclayton <43108818+marioclayton@users.noreply.github.com>
2026-03-15 23:12:42 -0400

    updated nextjs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.ctfassets.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
